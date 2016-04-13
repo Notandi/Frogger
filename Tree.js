bd40a707165d166baffd22adfc753bca8ccf23b8
Óskar <Notandi@users.noreply.github.com>
2016-04-13 16:28:08 +0000

    tree

--- Tree.js
+++ Tree.js
@@ -2,8 +2,6 @@ function Tree(descr){
 	this.setup(descr);
 }
 
-Tree.prototype.startLoc = 1.15;//vec3(1.15, -0.59, 6.0);
-Tree.prototype.endLoc = -12.15;//vec3(-12.15, -0.59, 6.0);
 Tree.prototype.loc;
 Tree.prototype.speed;
 Tree.prototype.xwidth = 2.0/2.0;
@@ -11,15 +9,6 @@ Tree.prototype.color;
 Tree.prototype.scale;
 
 Tree.prototype.update = function(du){
-	//console.log("log");
-    var dist = vec3(-this.speed*du,0.0,0.0);
-    this.loc = add(this.loc, dist);
-    if (this.loc[0] < this.endLoc){
-        this.loc[0] = this.startLoc;
-    }
-    if (this.loc[0] > this.startLoc){
-        this.loc[0] = this.endLoc;
-    }
 
 };
 
@@ -42,14 +31,14 @@ Tree.prototype.getXwidth = function(){
 
 Tree.prototype.render = function(gl){
 	//setja upp sjónarhornið 
-var mv = lookAt( eye, at, vec3(0.0, 1.0, 0.0) );
+    var mv = lookAt( eye, at, vec3(0.0, 1.0, 0.0) );
     mv = mult( mv, rotateX(spinX) );
     mv = mult( mv, rotateY(spinY) );
 
     // færa hlut
     var mv1 = mv;
+    console.log(this.loc);
     mv1 = mult(mv1, translate(this.loc));
-    mv1 = mult(mv1, scalem(0.5,0.5,0.5));
 
     mv1 = mult(mv1, scalem(this.scale));
 
@@ -61,14 +50,6 @@ var mv = lookAt( eye, at, vec3(0.0, 1.0, 0.0) );
     gl.uniformMatrix4fv(mvLoc, false, flatten(mv1));
     gl.drawArrays( gl.TRIANGLES, 0, numWaterVertices );
 
-
-    /*gl.uniform4fv( colorLoc, vec4(0.5, 0.5, 0, 1.0 ) );
-    gl.bindBuffer( gl.ARRAY_BUFFER, treeBuffer );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-
-    gl.uniformMatrix4fv(mvLoc, false, flatten(mv1));
-    gl.drawArrays( gl.TRIANGLE_STRIP, 0, numTreeVertices );*/
-    
     console.log("made it to render");
 
 
